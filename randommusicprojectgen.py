ad950e27df4d49cb9e7c2b54158bd12e36989f73
Micheal Cole Young <coleyoungmusic@gmail.com>
2017-05-20 01:02:56 -0500

    Update randommusicprojectgen.py

--- randommusicprojectgen.py
+++ randommusicprojectgen.py
@@ -4,24 +4,34 @@ import random
 
 app = gui()
 
-app.setFont(25)
-app.addLabelSpinBox("Key:", ['A', 'A#', 'B', 'C', 'C#', 'D',
-                            'D#', 'E', 'F', 'F#', 'G', 'G#'])
+Keys = ['A', 'A#', 'B', 'C', 'C#', 'D',
+       'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+Time_Signature = ['4/4', '3/4', '2/4', '6/8']
+
+BPM = list(range(50,201))
 
 app.setFont(25)
-app.addLabelSpinBox("Time Signature:", ['4/4', '3/4', '2/4', '6/8'])
+app.addLabelSpinBox("Key:", Keys)
 
 app.setFont(25)
-app.addLabelSpinBox("Bpm:", list(range(50,201)))
+app.addLabelSpinBox("Time Signature:", Time_Signature)
+
+app.setFont(25)
+app.addLabelSpinBox("BPM:", list(range(50,201)))
 
 
 def press(btn):
-    print(btn)
+    random_key = random.choice(Keys)
+    random_key2 = random.choice(Time_Signature)
+    random_key3 = random.choice(BPM)
+    app.setSpinBox("Key:", random_key, callFunction=True)
+    app.setSpinBox("Time Signature:", random_key2, callFunction=True)
+    app.setSpinBox("BPM:", random_key3, callFunction=True)
 
 
 app.addButton("Randomize", press)
 
 #Button will randomize the Key, BPM and Time Signature.
-
 
 app.go()
